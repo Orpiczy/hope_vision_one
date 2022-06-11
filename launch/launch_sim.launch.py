b45a21b6970b4d7aa98aa1ed56404fc38f3c8578
Lukas <orpiczymejl@gmail.com>
2022-06-11 23:51:36 +0200

    Adds custom neighbourhood world and world launch script

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -9,25 +9,31 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 
-
 def generate_launch_description():
-
 
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
 
-    package_name='hope_vision_one'
+    package_name = 'hope_vision_one'
 
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(
+                package_name), 'launch', 'rsp.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+    gazebo_custom_world = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(
+                package_name), 'launch', 'load_world_into_gazebo.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    # # Include the Gazebo launch file, provided by the gazebo_ros package
+    # gazebo = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource([os.path.join(
+    #         get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+    # )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
@@ -35,11 +41,10 @@ def generate_launch_description():
                                    '-entity', 'hope_vision_one'],
                         output='screen')
 
-
-
     # Launch them all!
     return LaunchDescription([
         rsp,
-        gazebo,
+        # gazebo,
+        gazebo_custom_world,
         spawn_entity,
     ])
